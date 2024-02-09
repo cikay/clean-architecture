232035242bc8c944e33c3cb41163cbd941c5a572
Muzaffer Cıkay <muzaffercky@gmail.com>
2024-02-09 19:55:09 +0300

    Set up db

--- entities/subject.py
+++ entities/subject.py
@@ -1,15 +1,21 @@
-from dataclasses import dataclass
+from datetime import datetime
+from dataclasses import dataclass, field
 
-from base_entity import BaseEntity
+
+from base_entity import BaseCreateEntity
 
 
 @dataclass
-class SubjectCreate(BaseEntity):
+class SubjectCreate(BaseCreateEntity):
     name: str
 
 
 @dataclass
-class Subject(SubjectCreate):
+class Subject:
     id: str
+    name: str
+    created_at: datetime = field(default_factory=datetime.now)
+    updated_at: datetime = field(default_factory=datetime.now)
+
 
     # domain business logic goes here
