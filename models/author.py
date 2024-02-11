3ab1f594ffcdb4eb694a3cf02f15e2234f0bbc33
Muzaffer Cıkay <muzaffercky@gmail.com>
2024-02-11 15:35:25 +0300

    Add author model

---
+++ models/author.py
@@ -0,0 +1,19 @@
+from peewee import CharField
+
+from models.base import BaseModel
+
+
+class AuthorBaseDB(BaseModel):
+    firstname = CharField()
+    lastname = CharField()
+
+    class Meta:
+        abstract = True
+
+
+class AuthorDB(AuthorBaseDB):
+    pass
+
+
+class TranslatorDB(AuthorBaseDB):
+    pass
