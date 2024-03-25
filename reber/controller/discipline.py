f45bfd1ae73ca5e18b3b59a0632810ff2e99a069
Muzaffer Cıkay <muzaffercky@gmail.com>
2024-03-25 22:27:52 +0300

    Fix discipline controllers and add tests

--- reber/controller/discipline.py
+++ reber/controller/discipline.py
@@ -1,3 +1,5 @@
+from dataclasses import asdict
+from reber.controller.base import GetController, GetManyController
 from reber.use_cases.discipline import (
     CreateDisciplineUseCase,
     GetDisciplineUseCase,
@@ -6,32 +8,35 @@ from reber.use_cases.discipline import (
 from reber.entities.discipline import DisciplineCreate
 
 
-class GetDisciplineController:
+class GetDisciplineController(GetController):
     def __init__(self, use_case: GetDisciplineUseCase):
         self.use_case = use_case
 
-    async def get(self, discipline_id) -> tuple[dict, int]:
+    async def execute(self, discipline_id) -> tuple[dict, int]:
         discipline = await self.use_case.execute(discipline_id)
-
         if not discipline:
             return {"detail": "discipline not found"}, 404
 
-        return discipline, 200
+        discipline_dict = asdict(discipline)
+        return discipline_dict, 200
 
 
-class GetManyDisciplineController:
+class GetManyDisciplineController(GetManyController):
     def __init__(self, use_case: GetManyDisciplineUseCase):
         self.use_case = use_case
 
-    async def get_many(self) -> tuple[dict, int]:
+    async def execute(self) -> tuple[dict, int]:
         disciplines = await self.use_case.execute()
-        return disciplines, 200
+        disciplines_dict = [asdict(discipline) for discipline in disciplines]
+        return disciplines_dict, 200
 
 
 class CreateDisciplineController:
     def __init__(self, use_case: CreateDisciplineUseCase):
         self.use_case = use_case
 
-    async def create(self, discipline: dict) -> tuple[dict, int]:
+    async def execute(self, discipline: dict) -> tuple[dict, int]:
         discipline_instance = DisciplineCreate(**discipline)
-        return await self.use_case.execute(discipline_instance), 201
+        discipline_instance = await self.use_case.execute(discipline_instance)
+        discipline_dict = asdict(discipline_instance)
+        return discipline_dict, 201
